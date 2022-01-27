b5640a74b106a0613b903147503a96f7f99af838
IlnurKam <kamal673@yandex.ru>
2022-01-27 21:47:44 +0300

    Добавьте файлы проекта.

---
+++ Zadanie13/Building.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework13
+{
+    class Building
+    {
+        public string adressHome { get; set; }
+        public string lengthHome { get; set; }
+        public string widthHome { get; set; }
+        public int height { get; set; }
+        static Building GetHome()
+        {
+            Building home = new Building();
+            home.adressHome = "Лубянка 37";
+            home.widthHome = "30";
+            home.lengthHome = "40";
+            home.height = 80;
+            return home;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Информация о здании");
+            Console.WriteLine($"Адрес: {adressHome}");
+            Console.WriteLine($"Длина: {lengthHome} м");
+            Console.WriteLine($"Ширина: {widthHome} м");
+            Console.WriteLine($"Высота: {height} м");
+        }
+    }
+}
---
+++ Zadanie13/MultiBuilding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework13
+{
+    class MultiBuilding : Building
+    {
+        public void floors()
+        {
+            Console.WriteLine("Этажность: 25");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiBuilding home = new MultiBuilding { adressHome = "Лубянка 37", lengthHome = "30", widthHome = "40", height = 80 };
+            home.PrintInfo();
+            home.floors();
+        }
+    }
+}
